008488e71afd27fe8c941051f3668bb4fda6d943
Sean Lee <seanboramlee@gmail.com>
2017-01-25 18:16:32 -0800

    add connector to BookDetail

--- src/containers/BookDetail.js
+++ src/containers/BookDetail.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+
+class BookDetail extends Component {
   render() {
     return (
       <div>Book Detail!</div>
     );
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail)
