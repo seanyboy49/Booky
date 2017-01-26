c8682348d8820cfc20e8e5bdddf9d0a939c1e8df
Sean Lee <seanboramlee@gmail.com>
2017-01-25 18:14:33 -0800

    wire Book Detail container to React

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -2,7 +2,7 @@
 // is responsible for
 export default function(state = null, action) {
   switch(action.type) {
-    case: 'BOOK_SELECTED':
+    case 'BOOK_SELECTED':
       return action.payload;
   }
   return state;
