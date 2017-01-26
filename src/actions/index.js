9702e9debbe9dd70d2de8c6041c78636ab5eaf3c
Sean Lee <seanboramlee@gmail.com>
2017-01-25 18:03:39 -0800

    create reducer_active_book.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreator that needs to return an action,
+  // and object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
 }
