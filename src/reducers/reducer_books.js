a18e3ff9b121d6f39690fe1005e91c0b582e80f8
Sean Lee <seanboramlee@gmail.com>
2017-01-25 18:24:17 -0800

    add pages property to books array

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'}
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 312},
+    { title: 'The Dark Tower', pages: 231},
+    { title: 'Eloquent Ruby', pages: 11}
   ]
 }
